17323b8408ed537879e5de674b7028ef55b9725b
jorge-am <117760161+jorge-am@users.noreply.github.com>
2022-11-09 12:56:44 -0600

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main (String[]args){
-		System.out.println("----Hello and welcome to Jenkins training----");
+		System.out.println("----Made some changes----");
 	}
 }
